984d79c5e1865953b4839e6c22d99b0198899361
officefish <fusikshukovky@gmail.com>
2025-02-08 20:26:25 +0300

    removing not used modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
 import { AppConfigModule } from 'src/modules/config/config.module';
 import { PingPongModule } from '@/modules/rest/ping-pong/ping-pong.module';
-import { AuthModule } from '@/modules/rest/auth/auth.module';
 
 
 @Module({
@@ -10,7 +9,6 @@ import { AuthModule } from '@/modules/rest/auth/auth.module';
     PrismaModule,
     AppConfigModule,
     PingPongModule,
-    AuthModule,   
   ],
   controllers: [],
   providers: [],
